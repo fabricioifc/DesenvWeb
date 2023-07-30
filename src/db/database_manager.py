881f255bbf703d19b361b626fa0a9ecdde680a46
Fabricio IF <fabricio.bizotto@ifc.edu.br>
2023-07-30 11:01:20 -0300

    secrets

--- src/db/database_manager.py
+++ src/db/database_manager.py
@@ -1,22 +1,21 @@
-import os
+import streamlit as st
 import sqlite3
-from dotenv import load_dotenv
-load_dotenv()
-
 from supabase import create_client
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # Load the environment variables
 
 # Get the Supabase URL and Key from the environment variables
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-SQLITE_DBNAME = os.getenv("SQLITE_DBNAME")
+# SUPABASE_URL = os.getenv("SUPABASE_URL")
+# SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+# SQLITE_DBNAME = os.getenv("SQLITE_DBNAME")
 
 def supabase_client():
-    return create_client(SUPABASE_URL, SUPABASE_KEY)
+    return create_client(**st.secrets["supabase"])
 
 def sqlite_client():
-    return sqlite3.connect(SQLITE_DBNAME)
+    return sqlite3.connect(st.secrets.sqlite.dbname)
 
 # class SQLiteManager(DatabaseManager):
 #     def __init__(self):
